b8ff9fdfaddb5c9de13b092c7129120f9b101ffb
danielCatlett <daniel.catlett42@gmail.com>
2017-01-20 10:01:32 -0600

    64th Birthday
    Calculates someone’s 64th birthday

---
+++ Contents.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+class SixtyFourthBirthday
+{
+    var birthYear = 0
+    var sixtyFourthYear = 0
+    
+    func setBirthYear(year: Int)
+    {
+        birthYear = year
+    }
+    
+    func addSixtyFour()
+    {
+        sixtyFourthYear = birthYear + 64
+    }
+}
